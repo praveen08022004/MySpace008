93ae1a0b1d3bbc72dad02efb377b950177cbd796
praveen08022004 <a.s.praveen2005@gmail.com>
2025-03-17 21:51:15 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <HashRouter>
+  <BrowserRouter basename="/praveenportfoliio">
     <App />
-  </HashRouter>
+  </BrowserRouter>
 );
