ae9b8f054a1c2eca5a04998b79d65fb1bbd64547
praveen08022004 <a.s.praveen2005@gmail.com>
2025-03-17 21:52:46 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <BrowserRouter basename="/praveenportfoliio">
-    <App />
-  </BrowserRouter>
+  <BrowserRouter basename="/MySpace008">
+  <App />
+</BrowserRouter>
 );
